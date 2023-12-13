df623182012179c7c7cd5775c42c031a05b6431e
Cameron <cameron.cw@outlook.com>
2023-12-13 19:33:09 +1000

    Search Now All Working

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-const filmsListEl = document.querySelector('.films-list')
-const title = localStorage.getItem("title")
+const filmsListEl = document.querySelector(".films-list");
 
 async function onSearchChange(event) {
-    const title = event.target.value
-    renderFilms(title)
+  const title = event.target.value;
+  renderFilms(title);
 }
 
-async function renderFilms(title) {
-    const films = await fetch(`http://www.omdbapi.com/?s=${title}&apikey=7f137b53`);
-    const filmsData = await films.json();
-    console.log(filmsData)
+async function renderFilms(title = "fast") {
+  const films = await fetch(
+    `http://www.omdbapi.com/?s=${title}&apikey=7f137b53`
+  );
+  const filmsData = await films.json();
+  console.log(filmsData);
+  filmsListEl.innerHTML = filmsData.Search.map((film) => filmsHTML(film)).join(
+    ""
+  );
 }
 
-function filmsHTML (films) {
-    return `
-    <div class="films">
-    <div class="films__title">
-      ${films.title}
-    </div>
-    <p class="films__body">
-      ${films.year}
-    </p>
-  </div>
-  `
+function filmsHTML(film) {
+  return `
+    <div class="film">
+              <figure class="film__img--wrapper">
+                <img
+                  class="film__img"
+                  src="${film.Poster}"
+                  alt=""
+                />
+              </figure>
+              <div class="films__title">${film.Title}</div>
+              <p class="films__body">Year: ${film.Year}</p>
+            </div>
+  `;
 }
 
-renderFilms(title)
+renderFilms();
